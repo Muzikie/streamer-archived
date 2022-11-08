89851636e50f8dc3c41fb61d8ecff206081761d6
Ali Haghighatkhah <ali@lightcurve.io>
2022-11-08 22:54:57 +0100

    Test endpoints

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,6 +1,14 @@
 const supertest = require('supertest');
 const app = require('../app');
 
+jest.mock('../config/api', () => ({
+  port: 4000,
+  songs: {
+    path: '../../../tests/songs/',
+    extension: 'mp3'
+  }
+}));
+
 describe('App', () => {
   describe('If API is up', () => {
     it('should return 200', (done) => {
@@ -8,15 +16,38 @@ describe('App', () => {
         .get('/status/api')
         .expect(200, done);
     });
+
+    it('should return 400 if any query parameter passed', () => {
+      return supertest(app)
+        .get('/status/api?foo=bar')
+        .expect(400);
+    })
   });
 
 
   describe('Audio', () => {
     describe('Stream audio' ,() => {
-      it('should return 404 if song does not exist', (done) => {
+      const validValue = 'cf434a889d6c7a064e8de61bb01759a76f585e5ff45a78ba8126ca332601f535';
+      const invalidValue = 'invalid';
+
+      it('should return 404 if endpoint does not exist', (done) => {
         supertest(app)
-          .get('/audio/stream/doesnotexist')
+          .get(`/audio/${invalidValue}?publicKey=${validValue}`)
+          .set('range', '0-10000')
           .expect(404, done);
+      });
+
+      it('should return 400 if audioID and publicKey are not a valid hash of length = 64', (done) => {
+        supertest(app)
+          .get(`/audio/${validValue}?publicKey=${invalidValue}`)
+          .expect(400, done);
+      });
+
+      it('should return 206 if audioID and publicKey are a valid hash of length = 64', (done) => {
+        supertest(app)
+          .get(`/audio/${validValue}?publicKey=${validValue}`)
+          .set('range', '0-10000')
+          .expect(206, done);
       });
     });
   });

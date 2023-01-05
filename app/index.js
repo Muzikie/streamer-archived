fc40748fb63d5a6eaf3c2c3fcc0370c13cf19991
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-05 12:14:16 +0100

    Simplify error handling

--- app/index.js
+++ app/index.js
@@ -2,31 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const ERRORS = require('./errors');
-const WHITE_LIST = require('./whiteList');
+const WHITE_LIST = require('../config/whiteList');
 
 const app = express();
 
 // white listed only our clients
 app.use(cors({
-  origin: WHITE_LIST,
+  origin: WHITE_LIST[0],
   optionsSuccessStatus: 200,
 }));
 
 app.use('/', routes);
 
-app.use((err, _req, res) => {
-  res.status(err.status || 400).json({
-    success: false,
-    message: err.message || ERRORS.UNHANDLED_ERROR,
-    errors: err.error || [],
-  });
-});
-
 app.use((_req, res) => {
-  res.status(404).json({
+  res.status(400).json({
     success: false,
-    message: ERRORS.RESOURCE_SUBSCRIPTION,
+    message: ERRORS.UNHANDLED_ERROR,
   });
 });
 
 module.exports = app;
+

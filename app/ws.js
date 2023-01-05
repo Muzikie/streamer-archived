e90d9ef4cb0ad3f69c06cbcd2f9dcb23d41051be
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-05 18:09:13 +0100

    Add onReady method

--- app/ws.js
+++ app/ws.js
@@ -3,18 +3,36 @@ const networkConfig = require('../config/network');
 
 class Socket {
   constructor () {
-    this.ws = null;
+    this.client = null;
+    this.readyCallbacks = [];
     this.create();
   }
 
   async create() {
     const url = `ws://${networkConfig.ws.ip}:${networkConfig.ws.port}/rpc-ws`;
-    const ws = await createWSClient(url);
-    this.ws = ws;
+    const client = await createWSClient(url);
+    this.client = client;
+    this.readyCallbacks.forEach(cb => cb(this.client));
   }
 
   request (endpoint, params) {
-    return this.ws.invoke(endpoint, params);
+    return this.client.invoke(endpoint, params);
+  }
+
+  subscribe (endpoint, cb) {
+    return this.client.subscribe(endpoint, cb);
+  }
+
+  onReady (cb) {
+    if (this.client !== null) {
+      cb(this.client);
+    } else {
+        this.readyCallbacks.push(cb);
+    }
+  }
+
+  disconnect () {
+    this.client.disconnect();
   }
 }
 

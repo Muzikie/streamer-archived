65be7a16c2885e792ea089a567d8c92ec7184f5c
Hamid HK <haghighat.cs@gmail.com>
2023-01-11 19:22:41 +0330

    Added complementary API features to users and collections

--- app/routes/collection/getAll.js
+++ app/routes/collection/getAll.js
@@ -1,9 +1,17 @@
 const Collection = require('../../models/collection');
+const APIFeatures = require('../../utils/apiFeatures');
 const { RESPONSE_STATUSES } = require('../../constants');
 
 exports.getAllCollections = async (req, res) => {
   try {
-    const data = await Collection.find();
+    const features = new APIFeatures(Collection.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const data = await features.query;
+
     res.status(200).json({
       status: RESPONSE_STATUSES.SUCCESS,
       data,

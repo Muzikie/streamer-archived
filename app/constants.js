8eb18fb93ef214a49e42027b8952e3f2fcba713d
Ali Haghighatkhah <ali@lightcurve.io>
2023-01-22 01:51:50 +0100

    Add a list of commands and modules and WS endpoints

--- app/constants.js
+++ app/constants.js
@@ -1,13 +1,44 @@
-exports.WS_MESSAGES = {
-  SUBSCRIPTION: {
-    HAS: 'subscription_hasSubscription',
-  }
+const modules = {
+  AUDIO: 'audio',
+  COLLECTION: 'collection',
+  PROFILE: 'profile',
+  SUBSCRIPTION: 'subscription',
 };
+exports.modules = modules;
+
+exports.commands = {
+  CREATE: 'create',
+  TRANSFER: 'transfer',
+  PURCHASE: 'purchase',
+  SET_ATTRIBUTES: 'setAttributes',
+  DESTROY: 'destroy',
+  STREAM: 'stream',
+};
+
+exports.idKeys = {
+  [modules.AUDIO]: 'audioID',
+  [modules.COLLECTION]: 'collectionID',
+  [modules.PROFILE]: 'profileID',
+  [modules.SUBSCRIPTION]: 'subscriptionID',
+}
+
+exports.WS_MESSAGES = {
+  subscription_hasSubscription: 'subscription_hasSubscription',
+  audio_getAccount: 'audio_getAccount',
+  audio_getAudio: 'audio_getAudio',
+  collection_getCollection: 'collection_getCollection',
+  collection_getAccount: 'collection_getAccount',
+  profile_getAccount: 'profile_getAccount',
+  profile_getProfile: 'profile_getProfile',
+};
+
 exports.HTTP_MESSAGES = {
   AUDIO_EXISTS: 'Audio already exists',
   COLLECTION_EXISTS: 'Collection already exists',
+  TRANSACTION_EXISTS: 'Transaction already exists',
   INVALID_SIGNATURE: 'Invalid signature',
 };
+
 exports.RESPONSE_STATUSES = {
   SUCCESS: 'success',
   ERROR: 'error',

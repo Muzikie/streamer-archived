2d378c198bc27c8dd729cc51083d8b21df9dbcdf
Hamid HK <haghighat.cs@gmail.com>
2023-02-19 23:10:40 +0330

    bug fix on stream.js

--- app/routes/audio/stream.js
+++ app/routes/audio/stream.js
@@ -10,56 +10,63 @@ const { RESPONSE_STATUSES } = require("../../constants");
 
 // eslint-disable-next-line max-statements, consistent-return
 exports.stream = async (req, res) => {
-  try {
-    //check if user has subscription
-    const { address } = getCookies(req.headers.cookie);
-    ws.request(WS_MESSAGES.subscription_hasSubscription, { address: address })
-      // eslint-disable-next-line max-statements
-      .then((response) => {
-        if (!response.success) {
-          return res.status(401).send(ERRORS.NO_SUBSCRIPTION);
+  //check if user has subscription
+  const { address } = getCookies(req.headers.cookie);
+  ws.request(WS_MESSAGES.subscription_hasSubscription, { address: address })
+    // eslint-disable-next-line max-statements
+    .then(async (response) => {
+      if (!response.success) {
+        return res.status(401).send(ERRORS.NO_SUBSCRIPTION);
+      }
+
+      try {
+        const audioID = req.params.id;
+        const range = req.headers.range;
+        //Find Audio in DB
+        await Audio.find({ audioID });
+        // /home/hamid/MUZIKIE/streamer/storage
+        const filePath = `${
+          path.join(__dirname, "../../../", AUDIOS.PATH) + audioID
+        }-audio.${AUDIOS.EXTENSION}`;
+
+        const stat = fs.statSync(filePath);
+        const fileSize = stat.size;
+
+        if (range) {
+          const parts = range.replace(/bytes=/, "").split("-");
+          const start = parseInt(parts[0], 10);
+          const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+          const chunkSize = end - start + 1;
+          const file = fs.createReadStream(filePath, { start, end });
+          const head = {
+            "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+            "Accept-Ranges": "bytes",
+            "Content-Length": chunkSize,
+            "Content-Type": "audio/mpeg",
+          };
+
+          res.writeHead(206, head);
+          file.pipe(res);
+        } else {
+          const head = {
+            "Content-Length": fileSize,
+            "Content-Type": "audio/mpeg",
+          };
+          res.writeHead(200, head);
+          fs.createReadStream(filePath).pipe(res);
         }
+      } catch (error) {
+        res.status(404).json({
+          status: RESPONSE_STATUSES.ERROR,
+          message: error.message,
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        status: RESPONSE_STATUSES.ERROR,
+        message: ERRORS.INTERNAL_SERVER_ERROR,
       });
-    const audioID = req.params.id;
-    const range = req.headers.range;
-    //Find Audio in DB
-    await Audio.find({ audioID });
-    // /home/hamid/MUZIKIE/streamer/storage
-    const filePath = `${
-      path.join(__dirname, "../../../", AUDIOS.PATH) + audioID
-    }-audio.${AUDIOS.EXTENSION}`;
-
-    const stat = fs.statSync(filePath);
-    const fileSize = stat.size;
-
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-      const chunkSize = end - start + 1;
-      const file = fs.createReadStream(filePath, { start, end });
-      const head = {
-        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunkSize,
-        "Content-Type": "audio/mpeg",
-      };
-
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        "Content-Length": fileSize,
-        "Content-Type": "audio/mpeg",
-      };
-      res.writeHead(200, head);
-      fs.createReadStream(filePath).pipe(res);
-    }
-  } catch (error) {
-    res.status(404).json({
-      status: RESPONSE_STATUSES.ERROR,
-      message: error.message,
     });
-  }
 };

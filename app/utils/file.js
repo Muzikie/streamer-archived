f8ba65f39ed358d7d1eda5bbe5fe06f9ba2b410c
Ali Haghighatkhah <ali@lightcurve.io>
2023-03-26 16:26:06 +0200

    Fix file name

--- app/utils/file.js
+++ app/utils/file.js
@@ -6,7 +6,7 @@ exports.getTypeByName = (fileName) => {
     case FILE_NAMES.AVATAR:
     case FILE_NAMES.BANNER:
       return '.jpg';
-    case FILE_NAMES.AUDIOS:
+    case FILE_NAMES.AUDIO:
       return '.mp3';
     default:
       throw new Error('Invalid file type');
